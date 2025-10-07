67d41c9eeef151c3e99e98b1e78ab9ce9ebd42fc
Aramez <191219550+Aramez-0@users.noreply.github.com>
2025-10-07 22:24:01 +1100

    improved basic game functionality

--- main.py
+++ main.py
@@ -9,8 +9,21 @@ obstacle_size = [8, 9]
 obstacle_gaps = 2
 obstacle_frequency = [3, 5]
 obstacles = []
-player = {"x": 0, "y": 0}
 prints_without_obstacles = 0
+player_lives = 1
+
+class player:
+
+	x = 0
+	y = 0
+	lives = player_lives
+	
+	def lose_lives(self, amount = 1) -> str:
+		self.lives -= 1
+		if self.lives <= 0:
+			return "dead\n"
+		else:
+			return "hit\n"
 
 def generate_obstacle():
 	obstacle = {
@@ -25,11 +38,11 @@ def generate_obstacle():
 		obstacle["gaps"].append(gap_num)
 	obstacles.append(obstacle)
 
-def print_board():
+def print_board(current_player):
 	os.system('cls' if os.name == 'nt' else 'clear')
 	global prints_without_obstacles
-	global player
 	global obstacles
+	message = ""
 	if prints_without_obstacles > obstacle_frequency[0] and prints_without_obstacles < obstacle_frequency[1]:
 		if random.randint(0, 2) == 0:
 			generate_obstacle()
@@ -45,29 +58,48 @@ def print_board():
 	board += "\n"
 	for k in range(board_length):
 		for i in range(board_width):
+			ob_placed = False
 			for j in obstacles:
-				if j["pos"] == i and not i in j["gaps"]:
+				if j["pos"] == i and not k in j["gaps"]:
 					board += "#"
-			if k == player["x"] and i == player["y"]:
+					ob_placed = True
+			if not ob_placed:
+				board += "/"
+			if k == current_player.x and i == current_player.y:
+				if ob_placed:
+					message += current_player.lose_lives()
 				board += "X"
 		board += "\n"
 	for i in range(board_width):
 		board += "#"
 	print(board)
-	player_input = input("Input: ")
-	if player_input == "s" and player["x"] < board_length:
-		player["x"] += 1
-	elif player_input == "w" and player["x"] >= 0:
-		player["x"] -= 1
-	elif player_input == "a" and player["y"] < board_width:
-		player["y"] += 1
-	elif player_input == "d" and player["y"] >= board_length:
-		player["y"] -= 1
-	elif player_input == "exit" or player_input == "quit" or player_input == "q":
-		exit()
+	print(message)
 	for i in obstacles:
 		i["pos"] += 1
 		if i["pos"] > board_width:
 			obstacles.remove(i)
-	print_board()
-print_board()
+	player_input = input("Input: ")
+	if player_input == "s" and current_player.x < board_length:
+		current_player.x += 1
+	elif player_input == "w" and current_player.x >= 0:
+		current_player.x -= 1
+	elif player_input == "a" and current_player.y < board_width:
+		current_player.y -= 1
+	elif player_input == "d" and current_player.y >= 0:
+		current_player.y += 1
+	elif player_input == "exit" or player_input == "quit" or player_input == "q":
+		exit()
+	if current_player.lives <= 0:
+		start_game()
+	print_board(current_player)
+
+def clear_board():
+	prints_without_obstacles = 0
+	obstacles.clear()
+
+def start_game():
+	clear_board()
+	current_player = player()
+	print_board(current_player)
+
+start_game()
